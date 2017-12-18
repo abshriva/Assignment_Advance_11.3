bca04ceaedac1b47f21eaf5b0c797a8e9f43c8c2
abshriva <shaky2108@gmail.com>
2017-12-18 13:39:47 +0530

    commit

---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,32 @@
+
+import { Component, OnInit,Input } from '@angular/core';
+import { Employee } from '../models/Employee';
+
+/**
+ * 
+ * 
+ * @export
+ * @class EmployeeDetailComponent
+ * @implements {OnInit}
+ */
+@Component({
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
+  
+})
+
+export class EmployeeDetailComponent implements OnInit {
+
+  @Input() empdetail;
+  imageUrl = 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg';
+  employeeList: Employee;
+  
+  
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
